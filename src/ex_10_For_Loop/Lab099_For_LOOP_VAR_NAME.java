190d9d1bcebb342300cd560247e01439fea82484
Abhishek Mahajan <amahajan338@gmail.com>
2025-06-17 21:45:21 +0530

    Lab of day 17 of Java training. For loop and While loop.

---
+++ src/ex_10_For_Loop/Lab099_For_LOOP_VAR_NAME.java
@@ -0,0 +1,10 @@
+package ex_10_For_Loop;
+
+public class Lab099_For_LOOP_VAR_NAME {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            //This will print 0-9
+        }
+    }
+}
---
+++ src/ex_10_For_Loop/Lab103_For_If_LOOP.java
@@ -0,0 +1,17 @@
+package ex_10_For_Loop;
+
+public class Lab103_For_If_LOOP {
+    public static void main(String[] args) {
+        for(int abhi = 0; abhi < 18; abhi++){
+            // 0 to 17, 18 times
+            if(abhi > 15){
+                System.out.println("Gift From Papa : iphone");
+            }else{
+                System.out.println("He will get only hugs! or Candy!");
+            }
+
+
+
+        }
+    }
+}
